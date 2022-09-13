da331da381bd346153731e786d1e05e3ea5b2b78
sanjeev singh <101657713+sanjeevsingh1976@users.noreply.github.com>
2022-09-13 17:30:00 +0530

    Delete main.js

--- emi calculator/main.js
+++
@@ -1,8 +0,0 @@
-//Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
-
-// Calculate results
-function calculateResults(e){
-    console.log('calculating')
-    e.preventDefault();
-}
