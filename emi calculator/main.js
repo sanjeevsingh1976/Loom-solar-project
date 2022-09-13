6930a227ba333b7b5bbbec10e8b1c751cca3115d
sanjeevsingh1976 <you@example.com>
2022-09-13 17:28:38 +0530

    first commit

---
+++ emi calculator/main.js
@@ -0,0 +1,8 @@
+//Listen for submit
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate results
+function calculateResults(e){
+    console.log('calculating')
+    e.preventDefault();
+}
